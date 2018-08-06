f30b5171555750629cce64e3d07ec898d9f78d96
Raphael ZEKRI <raphael.zekri@dougs.fr>
2018-08-06 14:06:51 +0200

    add material design

---
+++ TODO.md
@@ -0,0 +1,6 @@
+#TODO
+## Project creation
+```ng new [projectName]```
+
+## Add Angular material
+```npm install --save @angular/material @angular/cdk @angular/animations```
---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule
+    ]
+})
+export class MaterialModule {
+}
